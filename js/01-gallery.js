8b5f06acabf8989357148d77b9b8db9114a4ab1a
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-09 15:06:24 +0300

    Зад 1 создание разметки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// сщздать функцию для добавления разметки
+{
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+}
+
+function makeGalleryMarkup() {
+  return galleryItems
+    .map((param) => {
+      return `<div class="gallery__item">
+              <a class="gallery__link" href="large-image.jpg">
+                <img
+                  class="gallery__image"
+                  src="small-image.jpg"
+                  data-source="large-image.jpg"
+                  alt="Image description"
+                />
+             </a>
+           </div>`;
+    })
+    .join("");
+}
+console.log(makeGalleryMarkup(galleryItems));
+
+//const linkMakeImgGalleryRows = images.map(makeImgGalleryMarkup).join(" ");
+
+//gallerysEL.insertAdjacentHTML("beforeend", linkMakeImgGalleryRows);
