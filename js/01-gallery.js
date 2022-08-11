4b8a774c5eedb07d15227cadf7724becc1887994
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-11 20:41:18 +0300

    Зад 7 1 добавлено модальное окно,

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
 
 //ссылки на злементы
 const linkGallery = document.querySelector(".gallery");
 console.log(linkGallery);
 const galleryMarkup = makeGalleryMarkup(galleryItems);
-console.log(galleryMarkup);
+//console.log(galleryMarkup);
+
 linkGallery.insertAdjacentHTML("beforeend", galleryMarkup);
-linkGallery.addEventListener("click", onLinkgalleryActiv);
-function onLinkgalleryActiv(evt) {
-  console.log(evt);
-}
+linkGallery.addEventListener("click", (galleryItems) => {
+  galleryItems.preventDefault();
+
+  const oneGalleryItem = galleryItems.target;
+  console.log(oneGalleryItem);
+
+  console.log(oneGalleryItem.src);
+  console.log(galleryItems.original);
+  //oneGalleryItem.src = `${galleryItems.src.original}`;
+  //   const instance = basicLightbox.create(`
+  //     <img src="${original}" width="800" height="600">
+  // `);
+
+  //   instance.show();
+});
+
 // сщздать функцию для добавления разметки
 
 function makeGalleryMarkup() {
