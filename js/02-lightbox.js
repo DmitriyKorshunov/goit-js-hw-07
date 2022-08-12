448c81b6d562d26d60024d27f1d522b43a689566
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-12 13:48:34 +0300

    Зад 7 2 создана разметка с картинками

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const linkGallery = document.querySelector(".gallery");
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+linkGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+linkGallery.addEventListener("click", makeClickImg);
+
+function makeGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+              <a class="gallery__item" href="large-image.jpg">
+               <img class="gallery__image" src="${preview}" alt="${description}" />
+               </a>
+           </div>`;
+    })
+    .join("");
+}
