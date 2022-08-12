5be62c14dab9af05b0433a2ded742c2459c6fc90
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-12 14:13:15 +0300

    Зад 7 2 подключение файлов js и css библиотеки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,19 +3,30 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const linkGallery = document.querySelector(".gallery");
+console.log(linkGallery);
 const galleryMarkup = makeGalleryMarkup(galleryItems);
 
 linkGallery.insertAdjacentHTML("beforeend", galleryMarkup);
-linkGallery.addEventListener("click", makeClickImg);
+//linkGallery.addEventListener("click", makeClickImg);
 
 function makeGalleryMarkup() {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-              <a class="gallery__item" href="large-image.jpg">
+      return `<a class="gallery__item" href="large-image.jpg">
                <img class="gallery__image" src="${preview}" alt="${description}" />
-               </a>
-           </div>`;
+               </a>`;
     })
     .join("");
 }
+
+// function makeClickImg(galleryItems) {
+//   galleryItems.preventDefault();
+//   if (galleryItems.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(`
+//       <img src="${galleryItems.target.dataset.source}" width="800" height="600">
+//   `);
+
+//   instance.show();
+// }
